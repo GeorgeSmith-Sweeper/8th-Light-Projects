74c751551e53570954f0ab93cfc764836db7a219
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-08-25 14:21:17 -0400

    solved prime factors in45min NEW RECORD

--- prime-factors-kata/test_primeFactors.py
+++ prime-factors-kata/test_primeFactors.py
@@ -10,3 +10,15 @@ def test_returns_2_list_with_input_2():
 def test_returns_3_list_with_input_3():
 		assert computePrimeFactors(3) == [3]
 
+def test_returns_2_2_list_with_input_4():
+		assert computePrimeFactors(4) == [2,2]
+
+def test_returns_2_2_2_list_with_input_8():
+		assert computePrimeFactors(8) == [2,2,2]
+
+def test_returns_3_3_list_with_input_9():
+		assert computePrimeFactors(9) == [3,3]
+
+def test_returns_2_7_7_list_with_input_98():
+		assert computePrimeFactors(98) == [2,7,7]
+
