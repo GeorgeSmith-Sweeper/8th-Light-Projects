e9591849e2f73888b9c7aaeb5f5328abfda10140
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-08-25 14:12:21 -0400

    tests work up to 3

--- prime-factors-kata/primeFactors.py
+++ prime-factors-kata/primeFactors.py
@@ -1,5 +1,10 @@
 def computePrimeFactors(num):
-		if num == 1:
-			return []
-		if num == 2:
-			return [2]
+		prime_factors = []
+		current_prime = 2
+		while num > 1:
+				if num % current_prime == 0:
+						num = num / current_prime
+						prime_factors.append(current_prime)
+				else:
+						current_prime += 1
+		return prime_factors
--- prime-factors-kata/test_primeFactors.py
+++ prime-factors-kata/test_primeFactors.py
@@ -7,3 +7,6 @@ def test_returns_empty_list_with_input_1():
 def test_returns_2_list_with_input_2():
 		assert computePrimeFactors(2) == [2]
 
+def test_returns_3_list_with_input_3():
+		assert computePrimeFactors(3) == [3]
+
