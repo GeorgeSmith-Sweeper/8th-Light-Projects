5de8535208b022f195b180feee04db9651379217
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-08-29 14:10:18 -0400

    fresh start

--- prime-factors-kata/test_primeFactors.py
+++ prime-factors-kata/test_primeFactors.py
@@ -1,27 +1,3 @@
 import pytest
 from prime_factors import compute_prime_factors
 
-def test_returns_empty_list_when_value_is_1():
-		assert compute_prime_factors(1) == []
-
-def test_returns_2_list_when_value_is_2():
-		assert compute_prime_factors(2) == [2]
-
-def test_returns_3_list_when_value_is_3():
-		assert compute_prime_factors(3) == [3]
-
-def test_returns_2_2_list_when_value_is_4():
-		assert compute_prime_factors(4) == [2,2]
-
-def test_returns_2_3_list_when_value_is_6():
-		assert compute_prime_factors(6) == [2,3]
-
-def test_returns_2_2_2_list_when_value_is_8():
-		assert compute_prime_factors(8) == [2,2,2]
-
-def test_returns_2_5_list_when_value_is_10():
-		assert compute_prime_factors(10) == [2,5]
-
-def test_returns_2_7_7_list_when_value_is_98():
-		assert compute_prime_factors(98) == [2,7,7]
-
