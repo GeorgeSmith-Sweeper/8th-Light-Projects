9d1a26168168bd7a5f293631bbd8e43b10a2b4ee
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-08-30 13:28:16 -0400

    slightly different approach on tests

--- prime-factors-kata/prime_factors.py
+++ prime-factors-kata/prime_factors.py
@@ -1,2 +1,10 @@
 def compute_prime_factors(num):
-		return None
+		current_prime = 2
+		prime_factors = []
+		while num >= current_prime:
+				if num % current_prime == 0:
+						prime_factors.append(current_prime)
+						num /= 2
+				else:
+						current_prime += 1
+		return prime_factors
