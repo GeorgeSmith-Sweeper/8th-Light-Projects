da64e7c4b9cd256f445491ba1d482e0446a46520
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-08-31 11:08:23 -0400

    1 down

--- prime-factors-kata/compute_primes.py
+++ prime-factors-kata/compute_primes.py
@@ -1,3 +1,10 @@
 def find_factorial(num):
-    if num == 1:
-        return []
+    current_prime = 2
+    prime_factors = []
+    while num >= current_prime:
+        if num % current_prime == 0:
+            prime_factors.append(current_prime)
+            num /= current_prime
+        else: 
+            current_prime += 1
+    return prime_factors 
--- prime-factors-kata/test_computePrimes.py
+++ prime-factors-kata/test_computePrimes.py
@@ -4,3 +4,23 @@ from compute_primes import find_factorial
 def test_should_return_emty_list_with_1():
     assert find_factorial(1) == []
 
+def test_should_return_2_list_with_2():
+    assert find_factorial(2) == [2]
+
+def test_should_return_3_list_with_3():
+    assert find_factorial(3) == [3]
+
+def test_should_2_2_list_with_4():
+    assert find_factorial(4) == [2, 2]
+    
+def test_should_return_2_3_with_6():
+    assert find_factorial(6) == [2, 3]
+
+def test_should_return_2_5_with_10():
+    assert find_factorial(10) == [2, 5]
+    
+def test_should_2_7_with_21():
+    assert find_factorial(21) == [3, 7]
+
+def test_should_2_7_7_with_98():
+    assert find_factorial(98) == [2, 7, 7]
