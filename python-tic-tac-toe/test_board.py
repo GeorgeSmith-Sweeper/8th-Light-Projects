239ff93e14db212ef6d10f4ac1d6aebb789ca3fe
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-09-04 22:55:38 -0400

    display_board shows none is console

--- python-tic-tac-toe/test_board.py
+++ python-tic-tac-toe/test_board.py
@@ -7,6 +7,10 @@ class TestBoardSize(TestCase):
     def test_create_board_makes_list_with_9_elements(self):
         create_board()
         self.assertEqual(GameBoard.the_board, [None, None, None, None, None, None, None, None, None])
-
+'''
+ class TestDisplayBoard(TestCase):
+     def test_display_board_shows_blank_board(self):
+         self.assertEqual(display_board(), )
+'''
 if __name__ == "__main__":
     unittest.main()
