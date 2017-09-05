bd5531e60d5ca9c02bd3686d4c9c137051eda5f4
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-09-05 14:36:55 -0400

    git push

--- prime-factors-kata/compute_primes.py
+++ prime-factors-kata/compute_primes.py
@@ -1,10 +1,8 @@
 def find_factorial(num):
     prime_factors = []
-    current_prime = 2
-    while num >= current_prime: 
-        if num % current_prime == 0:
-            num /= current_prime 
-            prime_factors.append(current_prime)
-        else:
-            current_prime += 1
-    return prime_factors
+    if num >= 2:
+        if num % 2 == 0:
+            return [2]
+        if num % 3 == 0:
+            return [3]
+    return []
